e08fd90b3cd0b1c1b19ee453234ff69b62308a0a
nojimage <nojimage@gmail.com>
2018-02-16 16:56:25 +0900

    rewrite Bootstrap command

--- tests/TestCase/Command/BootstrapTest.php
+++ tests/TestCase/Command/BootstrapTest.php
@@ -10,11 +10,16 @@ class BootstrapTest extends TestCase
     {
         $testSuiteDir = TEST_APP_ROOT . 'src' . DS . 'TestSuite' . DS . 'Codeception' . DS;
         $this->assertTrue(is_dir($testSuiteDir), 'src/TestSuite/Codeception directory must be auto-created');
-        // @codingStandardsIgnoreStart
-        $this->assertTrue(file_exists($testSuiteDir . 'AcceptanceHelper.php'), 'AcceptanceHelper must be auto-created');
-        $this->assertTrue(file_exists($testSuiteDir . 'FunctionalHelper.php'), 'FunctionalHelper must be auto-created');
-        // @codingStandardsIgnoreEnd
-        $this->assertTrue(file_exists($testSuiteDir . 'UnitHelper.php'), 'UnitHelper must be auto-created');
+
+        $this->assertTrue(file_exists($testSuiteDir . 'Helper/Acceptance.php'), 'AcceptanceHelper must be auto-created');
+        $this->assertTrue(file_exists($testSuiteDir . 'Helper/Functional.php'), 'FunctionalHelper must be auto-created');
+        $this->assertTrue(file_exists($testSuiteDir . 'Helper/Unit.php'), 'UnitHelper must be auto-created');
+
+        $this->assertTrue(file_exists($testSuiteDir . 'AcceptanceTester.php'), 'AcceptanceTester must be auto-generated');
+        $this->assertTrue(file_exists($testSuiteDir . 'FunctionalTester.php'), 'FunctionalTester must be auto-generated');
+        $this->assertTrue(file_exists($testSuiteDir . 'UnitTester.php'), 'UnitTester must be auto-generated');
+
+        $this->assertTrue(file_exists($testSuiteDir . '_generated/.gitignore'), '_generated directory .gitignore must be auto-created');
     }
 
     public function testSuitesCreatedInTests()
@@ -22,26 +27,16 @@ class BootstrapTest extends TestCase
         $testsDir = TEST_APP_ROOT . 'tests' . DS;
 
         $acceptanceDir = $testsDir . 'Acceptance' . DS;
-        $this->assertTrue(is_dir($acceptanceDir), 'Acceptance test suite directory must be auto-created');
-        // @codingStandardsIgnoreStart
-        $this->assertTrue(file_exists($acceptanceDir . 'AcceptanceTester.php'), 'AcceptanceTester must be auto-generated');
-        $this->assertTrue(file_exists($acceptanceDir . 'bootstrap.php'), 'Acceptance bootstrap.php must be auto-created');
-        // @codingStandardsIgnoreEnd
-        $this->assertTrue(file_exists($acceptanceDir . '.gitignore'), 'Acceptance .gitignore must be auto-created');
-
+        $this->assertDirectoryExists($acceptanceDir, 'Acceptance test suite directory must be auto-created');
+        $this->assertFileExists($acceptanceDir . 'bootstrap.php', 'Acceptance bootstrap.php must be auto-created');
+        
         $functionalDir = $testsDir . 'Functional' . DS;
-        $this->assertTrue(is_dir($functionalDir), 'Functional test suite directory must be auto-created');
-        // @codingStandardsIgnoreStart
-        $this->assertTrue(file_exists($functionalDir . 'FunctionalTester.php'), 'FunctionalTester must be auto-generated');
-        $this->assertTrue(file_exists($functionalDir . 'bootstrap.php'), 'Functional bootstrap.php must be auto-created');
-        // @codingStandardsIgnoreEnd
-        $this->assertTrue(file_exists($functionalDir . '.gitignore'), 'Functional .gitignore must be auto-created');
+        $this->assertDirectoryExists($functionalDir, 'Functional test suite directory must be auto-created');
+        $this->assertFileExists($functionalDir . 'bootstrap.php', 'Functional bootstrap.php must be auto-created');
 
         $unitDir = $testsDir . 'Unit' . DS;
-        $this->assertTrue(is_dir($unitDir), 'Unit test suite directory must be auto-created');
-        $this->assertTrue(file_exists($unitDir . 'UnitTester.php'), 'UnitTester must be auto-generated');
-        $this->assertTrue(file_exists($unitDir . 'bootstrap.php'), 'Unit bootstrap.php must be auto-created');
-        $this->assertTrue(file_exists($unitDir . '.gitignore'), 'Unit .gitignore must be auto-created');
+        $this->assertDirectoryExists($unitDir, 'Unit test suite directory must be auto-created');
+        $this->assertFileExists($unitDir . 'bootstrap.php', 'Unit bootstrap.php must be auto-created');
     }
 
     public function testCoreConfigurationFileCreated()
@@ -51,6 +46,8 @@ class BootstrapTest extends TestCase
 
         $result = file_get_contents($configFilePath);
         $this->assertNotContains('dsn', $result, 'No dsn configuration should be included');
+        $this->assertContains('namespace: App\TestSuite\Codeception', $result, 'namespace must be enabled');
+        $this->assertContains('bootstrap: bootstrap.php', $result, 'bootstrap must be enabled');
     }
 
     public function testAcceptanceConfigurationFileCreated()
@@ -59,7 +56,8 @@ class BootstrapTest extends TestCase
         $this->assertTrue(file_exists($configFilePath), 'File `acceptance.suite.yml` must be auto-created');
 
         $result = file_get_contents($configFilePath);
-        $this->assertContains('Cake\Codeception\Helper', $result, 'Cake helper module must be enabled');
+        $this->assertContains('\Cake\Codeception\Helper', $result, 'Cake helper module must be enabled');
+        $this->assertContains('\App\TestSuite\Codeception\Helper\Acceptance', $result, 'Acceptance helper must be enabled');
     }
 
     public function testFunctionalConfigurationFileCreated()
@@ -68,7 +66,8 @@ class BootstrapTest extends TestCase
         $this->assertTrue(file_exists($configFilePath), 'File `functional.suite.yml` must be auto-created');
 
         $result = file_get_contents($configFilePath);
-        $this->assertContains('Cake\Codeception\Helper', $result, 'Cake helper module must be enabled');
+        $this->assertContains('\Cake\Codeception\Helper', $result, 'Cake helper module must be enabled');
+        $this->assertContains('\App\TestSuite\Codeception\Helper\Functional', $result, 'Functional helper must be enabled');
     }
 
     public function testUnitConfigurationFileCreated()
@@ -77,6 +76,8 @@ class BootstrapTest extends TestCase
         $this->assertTrue(file_exists($configFilePath), 'File `unit.suite.yml` must be auto-created');
 
         $result = file_get_contents($configFilePath);
-        $this->assertNotContains('Cake\Codeception\Helper', $result, 'Cake helper module must not be enabled');
+        $this->assertNotContains('\Cake\Codeception\Helper', $result, 'Cake helper module must not be enabled');
+        $this->assertContains('\Cake\Codeception\Framework', $result, 'Cake framework module must be enabled');
+        $this->assertContains('\App\TestSuite\Codeception\Helper\Unit', $result, 'Unit helper must be enabled');
     }
 }
