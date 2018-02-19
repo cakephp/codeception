a356795ea27cfc7f6b5ae88d988f30e968f2a33d
nojimage <nojimage@gmail.com>
2018-02-19 16:05:53 +0900

    support Application/Server runner for CakePHP 3.5

--- src/Connector.php
+++ src/Connector.php
@@ -57,7 +57,7 @@ class Connector extends Client
      * @param \Symfony\Component\BrowserKit\Request $request BrowserKit request.
      * @return \Cake\Network\Request Cake request.
      */
-    protected function filterRequest(\Symfony\Component\BrowserKit\Request $request)
+    protected function filterRequest(BrowserKitRequest $request)
     {
         $url = preg_replace('/^https?:\/\/[a-z0-9\-\.]+/', '', $request->getUri());
 
@@ -72,11 +72,15 @@ class Connector extends Client
             'environment' => $environment,
         ];
 
-        $this->cake['request'] = new Request($props);
+        if (class_exists('\Cake\Http\ServerRequest')) {
+            $this->cake['request'] = new \Cake\Http\ServerRequest($props);
+        } else {
+            $this->cake['request'] = new Request($props);
 
-        // set params
-        Router::setRequestInfo($this->cake['request']);
-        $this->cake['request']->params = Router::parse($url);
+            // set params
+            Router::setRequestInfo($this->cake['request']);
+            $this->cake['request']->params = Router::parse($url);
+        }
 
         return $this->cake['request'];
     }
@@ -120,23 +124,60 @@ class Connector extends Client
     protected function doRequest($request)
     {
         $response = new Response();
+        $applicationClass = '\\' . Configure::read('App.namespace') . '\Application';
 
+        try {
+            if (class_exists('\Cake\Http\Server') && class_exists($applicationClass)) {
+                $response = $this->runApplication($applicationClass, $request);
+            } else {
+                $response = $this->runDispatcher($request, $response);
+            }
+        } catch (\PHPUnit_Exception $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            $response = $this->handleError($e);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Run application CakePHP < 3.3
+     *
+     * @param \Cake\Network\Request $request Cake request.
+     * @return \Cake\Network\Response Cake response.
+     */
+    protected function runDispatcher($request, $response)
+    {
         $dispatcher = DispatcherFactory::create();
         $dispatcher->eventManager()->on(
             'Dispatcher.beforeDispatch',
             ['priority' => 999],
             [$this, 'controllerSpy']
         );
+        ob_start();
+        $dispatcher->dispatch($request, $response);
+        ob_end_clean();
 
-        try {
-            ob_start();
-            $dispatcher->dispatch($request, $response);
-            ob_end_clean();
-        } catch (\PHPUnit_Exception $e) {
-            throw $e;
-        } catch (\Exception $e) {
-            $response = $this->handleError($e);
-        }
+        return $response;
+    }
+
+    /**
+     * Run application CakePHP > 3.3
+     *
+     * @param string $applicationClass
+     * @return \Cake\Http\Response Cake response.
+     */
+    protected function runApplication($applicationClass, $request)
+    {
+        $server = new \Cake\Http\Server(new $applicationClass(CONFIG));
+
+        $server->eventManager()->on(
+            'Dispatcher.beforeDispatch',
+            ['priority' => 999],
+            [$this, 'controllerSpy']
+        );
+        $response = $server->run($request);
 
         return $response;
     }
