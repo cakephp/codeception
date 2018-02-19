4e9d236d4909f5a7ee8f9f3603ccfd15979df532
nojimage <nojimage@gmail.com>
2018-02-19 14:01:06 +0900

    remove sample_app source files

--- tests/sample_app/src/TestSuite/Codeception/Helper/Functional.php
+++ tests/sample_app/src/TestSuite/Codeception/Helper/Functional.php
@@ -1,11 +1,10 @@
 <?php
 namespace App\TestSuite\Codeception\Helper;
 
-use Codeception\Module;
-
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class Functional extends Module
+class Functional extends \Codeception\Module
 {
+
 }
