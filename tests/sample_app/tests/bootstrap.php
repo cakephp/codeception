b75965275d7c537c48977de5cc1bd4da8712fa9c
nojimage <nojimage@gmail.com>
2018-02-19 12:28:59 +0900

    fixes class conflict on PHPUnit >= 6.0

--- tests/sample_app/tests/bootstrap.php
+++ tests/sample_app/tests/bootstrap.php
@@ -5,4 +5,12 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+// fixes class conflict on PHPUnit >= 6.0
+if (class_exists('\PHPUnit\Runner\Version') && version_compare(\PHPUnit\Runner\Version::id(), '6.0', '>=') &&
+    !function_exists('loadPHPUnitAliases')) {
+    function loadPHPUnitAliases() {
+        // nothing to do
+    }
+}
+
 require dirname(__DIR__) . '/config/bootstrap.php';
